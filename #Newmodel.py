d747e6defc00736bcf621b23e51a009143ef7fdf
Mswarankit <ankitnightr14@gmail.com>
2022-06-28 22:07:33 +0530

    2nd change

--- #Newmodel.py
+++ #Newmodel.py
@@ -1,7 +1,15 @@
-#Newmodel
+#parameters
+import numpy as np
 
-print('Hello for this only')
+L = 1000 # aquifer length, m
+H = 10 # aquifer thickness, m
+zb = -6 #aquifer bottom, m
+k = 10 # hudraulic conductivity
+n = 0.3 #porisity
+T = k * H
+h0 = 6
+hL = 4
 
-print('happy')
-
-
+x = np.linespace(0, L, 100)
+h = (hL - h0) * x/L + h0
+Qx = - T * (hL - h0) / L * np.ones_like(x)
