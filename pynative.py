74cc89b4e2182e42403ff42e4107f4a9ba366f6c
Ankit Mishra(Mswarankit) <42618654+Mswarankit@users.noreply.github.com>
2022-11-21 07:29:02 +0000

    2nd change

--- pynative.py
+++ pynative.py
@@ -32,14 +32,54 @@
 
 # print(num)
 
-num = [12, 75, 243, 281, 9, 31, 790, 901, 1252]
-for i in num:
-    if i > 790:
-        continue
-    elif i < 1252:
-        break
-    elif i % 3 == 0:
-        print(i)
+# num = [12, 75, 243, 281, 9, 31, 790, 901, 1252]
+# for i in num:
+#     if i > 790:
+#         continue
+#     elif i < 1252:
+#         break
+#     elif i % 3 == 0:
+#         print(i)
     
 
-print(num)
+# # print(num)
+# num = int(input())
+# counter = 0
+# while num != 0:
+#     num = num // 10 # floor division of the number so only integer value I will recieve 
+#     counter+=1
+
+# print(counter)
+
+# num = (input())
+# length = len(num)
+# print(length)
+# len function only work with str
+
+# let's check that the number is palindrome
+# 1. take the input of num 
+# 2. get the reverse the len
+
+# reverse the number
+# n = int(input())
+# for i in range(n, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+    # print()
+
+
+# print list in reverse order
+# list1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# size = len(list1)
+# for i in range(size -1, 0, -1):
+#     print(list1[i])
+
+list1 = [i*x for i in range(5,2, -1) for x in range(1,11)]
+print(list1)
+list1.reverse()
+print(list1)
+
+# li = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+# li.reverse()
+# print(li)
+
